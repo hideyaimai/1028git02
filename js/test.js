35e984981546785dc33ac5939f4cd0eada8e8c6c
hideyaimai <hideya.imai.biz@gmail.com>
2023-10-31 00:25:10 +0900

    課題の内容を移動しました。

---
+++ js/test.js
@@ -0,0 +1,79 @@
+let playerhands = "";
+let computerhands = "";
+
+$(".pickgu,.pickchoki,.pickpa").on("click",function () {
+    // ここに処理を書く
+    let random = Math.floor(Math.random()*3);
+    let imagepath;
+
+    if(random === 0){
+        $("h2").html("グー");
+        imagepath = "img/janken_gu.png";
+        computerhands = "グー";
+    }
+    else if(random === 1){
+        $("h2").html("チョキ");
+        imagepath = "img/janken_choki.png";
+        computerhands = "チョキ";
+    }
+    else if(random === 2){
+        $("h2").html("パー");
+        imagepath = "img/janken_pa.png";
+        computerhands = "パー";
+    }
+    $(".items").attr("src",imagepath);
+})
+
+$(".pickgu").on("click",function(){
+    $(".myhandsname").html("グー")
+    $(".myhandpicture").attr("src", "img/janken_gu.png");
+    playerhands = "グー";
+})
+
+$(".pickchoki").on("click",function(){
+    $(".myhandsname").html("チョキ")
+    $(".myhandpicture").attr("src", "img/janken_choki.png");
+    playerhands = "チョキ";
+})
+
+$(".pickpa").on("click",function(){
+    $(".myhandsname").html("パー")
+    $(".myhandpicture").attr("src", "img/janken_pa.png");
+    playerhands = "パー";
+})
+
+$(".pickpa").on("click",function() {
+    if(computerhands === "グー"){
+        $(".resulttext").html("勝ち")
+    }
+    else if(computerhands === "チョキ"){
+        $(".resulttext").html("負け")
+    }
+    else{
+        $(".resulttext").html("あいこ")
+    }
+});
+
+$(".pickgu").on("click",function() {
+    if(computerhands === "グー"){
+        $(".resulttext").html("あいこ")
+    }
+    else if(computerhands === "チョキ"){
+        $(".resulttext").html("勝ち")
+    }
+    else{
+        $(".resulttext").html("負け")
+    }
+});
+
+$(".pickchoki").on("click",function() {
+    if(computerhands === "グー"){
+        $(".resulttext").html("負け")
+    }
+    else if(computerhands === "チョキ"){
+        $(".resulttext").html("あいこ")
+    }
+    else{
+        $(".resulttext").html("勝ち")
+    }
+});
